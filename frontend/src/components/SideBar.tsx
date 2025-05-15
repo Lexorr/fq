3dd1220511c1516278e4ea46f70a469bd2a93b3c
fq <ventureslexor@gmail.com>
2025-05-15 01:44:19 -0700

    correctionn

--- frontend/src/components/SideBar.tsx
+++ frontend/src/components/SideBar.tsx
@@ -29,7 +29,7 @@ const SideBar = () => {
       {/* Sidebar */}
       <aside
         className={`fixed top-0 left-0 h-screen w-64 bg-gradient-to-b from-gray-900 to-blue-900 text-white z-40 transform transition-transform duration-300 ease-in-out 
-        ${isOpen ? 'translate-x-0' : '-translate-x-full'} md:translate-x-0`}
+        ${isOpen ? 'translate-x-0' : '-translate-x-full'} md:translate-x-0`} onClick={toggleSidebar}
       >
         {/* Top Section */}
         <div className="p-6">
