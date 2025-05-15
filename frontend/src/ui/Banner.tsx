7060a1bc8c58a167754ac83d9281077d189a9c7f
fq <ventureslexor@gmail.com>
2025-05-15 03:11:16 -0700

    ui

--- frontend/src/ui/Banner.tsx
+++ frontend/src/ui/Banner.tsx
@@ -8,12 +8,12 @@ interface BannerProps {
 
 const Banner: React.FC<BannerProps> = ({ title, subtitle,  }) => {
   return (
-    <div className="bg-gradient-to-r from-gray-800 to-gray-900 text-white px-6 py-10 flex flex-col md:flex-row items-center justify-between mt-15 md:mt-18">
+    <div className="bg-gradient-to-r from-gray-800 to-gray-900 text-white px-5 py-5 flex flex-col md:flex-row  justify-between mt-15 md:mt-18">
       {/* Text Section */}
-      <div className="text-center md:text-left">
-        <h1 className="text-5xl sm:text-8xl md:text-5xl font-bold">{title}</h1>
+      <div className="md:text-left">
+        <h1 className="md:text-[30px] text-[10px]  font-bold">{title}</h1>
         {subtitle && (
-          <p className="text-gray-300 mt-2 text-lg">{subtitle}</p>
+          <p className="text-gray-300  text-[8px] md:text-30px]">{subtitle}</p>
         )}
       </div>
 
