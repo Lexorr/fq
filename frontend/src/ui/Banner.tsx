dc4f2552d64fa360f82a370468452b8a5daf72a8
fq <ventureslexor@gmail.com>
2025-05-15 03:15:10 -0700

    ui

--- frontend/src/ui/Banner.tsx
+++ frontend/src/ui/Banner.tsx
@@ -6,15 +6,13 @@ interface BannerProps {
   
 }
 
-const Banner: React.FC<BannerProps> = ({ title, subtitle,  }) => {
+const Banner: React.FC<BannerProps> = ({ title,  }) => {
   return (
     <div className="bg-gradient-to-r from-gray-800 to-gray-900 text-white px-5 py-5 flex flex-col md:flex-row  justify-between mt-15 md:mt-18">
       {/* Text Section */}
       <div className="md:text-left">
         <h1 className="md:text-[30px] text-[10px]  font-bold">{title}</h1>
-        {subtitle && (
-          <p className="text-gray-300  text-[8px] md:text-30px]">{subtitle}</p>
-        )}
+      
       </div>
 
       {/* Image Section */}
